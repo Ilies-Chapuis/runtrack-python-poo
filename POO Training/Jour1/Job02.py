73e51ad896862a37065927c49830b15b2ea9a8ea
Ilies-Chapuis <ilies.chapuis@laplateforme.io>
2026-02-02 12:00:10 +0100

    Job02 done

---
+++ POO Training/Jour1/Job02.py
@@ -0,0 +1,8 @@
+class Operation:
+    def __init__(self, nombre1=12, nombre2=3):
+        self.nombre1 = 12
+        self.nombre2 = 3
+
+Michel = Operation()
+print(f"Le nombre1 est {Michel.nombre1}")
+print(f"Le nombre2 est {Michel.nombre2}")
