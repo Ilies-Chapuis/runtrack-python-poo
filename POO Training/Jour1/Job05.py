89c15f39e68a7002f30f480aae464c68c59a422f
Ilies-Chapuis <ilies.chapuis@laplateforme.io>
2026-02-03 10:10:04 +0100

    Job05 not finished

---
+++ POO Training/Jour1/Job05.py
@@ -0,0 +1,27 @@
+class Point:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+    def afficherLesPoints(self, x, y):
+        print(f"{self.x} {self.y}")
+
+
+    def afficherX(self):
+        print(f"{self.x}")
+
+
+    def afficherY(self):
+        print(f"{self.y}")
+
+
+    def changerX(self):
+
+
+
+    def changerY(self):
+
+
+Po = Point()
